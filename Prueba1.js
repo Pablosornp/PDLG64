ac2477761860ebde49bf4e4943ca63581be0390f
Pablosornp <pablo.osorno.94@gmail.com>
2018-10-22 19:08:13 +0200

    añadida la libreria PLY, main en lexico.ply y un fichero fuente ejemplo

---
+++ Prueba1.js
@@ -0,0 +1,1 @@
+var e1efefa = 5;
--- lexico.py
+++ lexico.py
@@ -15,95 +15,112 @@ tokens = reservadas + ['ID','ENT','CAD','MAS','MENOS','MUL','DIV','AND','LLAVA',
 
 #Expresiones Regulares
 
-		t_MAS = r'\+'
-		t_MENOS = r'-'
-		t_MUL = r'\*'
-		t_Div = r'/'
+t_MAS = r'\+'
+t_MENOS = r'-'
+t_MUL = r'\*'
+#t_Div = r'/'
 
+t_ignore = '\t '
+t_LLAVA = r'{'
+t_LLAVC = r'}'
+t_PARA = r'\('
+t_PARC = r'\)'
+t_CORA = r'\['
+t_CORC = r'\]'
 
-		t_LLAVA = r'{'
-		t_LLAVC = r'}'
-		t_PARA = r'\('
-		t_PARC = r'\)'
-		t_CORA = r'\['
-		t_CORC = r'\]'
+t_FIN = r';'
+t_SIG = r','
 
-		t_FIN = r';'
-		t_SIG = r','
-
-		#t_OR = r'\'
-		t_AND = r'\&\&'
-		t_NOT = r'\!'
-		#t_ASIG_R = r'\-\='
-		t_ASIG = r'='
-		t_OP_MAY = r'>'
-		t_OP_MEN = r'<'
-		#t_OP_MAY_IG = r'\>\=' 
-		#t_OP_MEN_IG = r'\<\='
-		#t_IG = r'\=\='
+#t_OR = r'\'
+t_AND = r'\&\&'
+t_NOT = r'\!'
+#t_ASIG_R = r'\-\='
+t_ASIG = r'='
+t_OP_MAY = r'>'
+t_OP_MEN = r'<'
+#t_OP_MAY_IG = r'\>\='
+#t_OP_MEN_IG = r'\<\='
+#t_IG = r'\=\='
 
 
 
 #Funciones
-		
-		def t_ID(c):
-			r'\w+(_\d\w)'
-			return c
 
-		def t_ENT(c):
-			r'\d+'
-			c.value=int(c.value)
-			return c
+def t_ID(c):
+	r'[a-zA-Z_]+(_\d\w)*'
+	return c
 
-		def t_INT(c):
-			r'int'
-			return c
+def t_ENT(c):
+	r'\d+'
+	c.value=int(c.value)
+	return c
 
-		def t_BOOL(c):
-			r'bool'
-			return c
+def t_INT(c):
+	r'int'
+	return c
 
-		def t_STRING(c):
-			r'string'
-			return c
+def t_BOOL(c):
+	r'bool'
+	return c
 
-		def t_IF(c):
-			r'if'
-			return c
+def t_STRING(c):
+	r'string'
+	return c
 
-		def t_DEFAULT(c):
-			r'dfault'
-			return c
+def t_IF(c):
+	r'if'
+	return c
 
-		def t_BREAK(c):
-			r'break'
-			return c
+def t_DEFAULT(c):
+	r'dfault'
+	return c
 
-		def t_RETURN(c):
-			r'return'
-			return c
+def t_BREAK(c):
+	r'break'
+	return c
 
-		def t_FUNCTION(c):
-			r'function'
-			return c
+def t_RETURN(c):
+	r'return'
+	return c
 
-		def t_VAR(c):
-			r'var'
-			return c
+def t_FUNCTION(c):
+	r'function'
+	return c
 
-		def t_SWITCH(c):
-			r'switch'
-			return c
+def t_VAR(c):
+	r'var'
+	return c
 
-		def t_CASE(c):
-			r'case'
-			return c
+def t_SWITCH(c):
+	r'switch'
+	return c
 
-		def t_PRINT(c):
-			r'print'
-			return c
+def t_CASE(c):
+	r'case'
+	return c
 
-		def t_PROMPT(c):
-			r'promt'
-			return c
+def t_PRINT(c):
+	r'print'
+	return c
 
+def t_PROMPT(c):
+	r'promt'
+	return c
+
+def t_error(t):
+	print "caracter ilegal '%s'" % t.value[0]
+	t.lexer.skip(1)
+
+analizador = lex.lex()
+
+nombreFichero = raw_input("Inserta nombre de fichero:")
+handle = open(nombreFichero)
+cadena =handle.read()
+analizador.input(cadena)
+
+while True:
+	tok = analizador.token()
+	if not tok :
+		print("Token erroneo:",tok)
+		break
+ 	print tok
